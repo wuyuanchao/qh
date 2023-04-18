d2c7470066e3900b97d53a3a55b292a159a83ed8
wuyuanchao <fourier@jollycorp.com>
2023-04-19 01:02:40 +0800

    对接询价单接口

--- qh-controller/src/main/java/com/chic/qh/controller/enquiry/Inquiry.java
+++ qh-controller/src/main/java/com/chic/qh/controller/enquiry/Inquiry.java
@@ -1,19 +1,15 @@
 package com.chic.qh.controller.enquiry;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Inquiry {
+    private Integer id;
     private String name;
     private String customer;
     private String createdAt;
-
-    public Inquiry() {
-    }
-
-    public Inquiry(String name, String customer, String createdAt) {
-        this.name = name;
-        this.customer = customer;
-        this.createdAt = createdAt;
-    }
 }
