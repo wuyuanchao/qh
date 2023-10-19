1a4b7ea44624e36a22ec1fbf1627be13454dafd2
bombzj <bombzj@gmail.com>
2023-10-19 17:19:25 +0800

    优化解析excel日期时间格式

--- src/main/java/com/chic/qh/support/utils/ExcelDate2TimestampConverter.java
+++ src/main/java/com/chic/qh/support/utils/ExcelDate2TimestampConverter.java
@@ -1,5 +1,8 @@
 package com.chic.qh.support.utils;
 
+import java.sql.Date;
+import java.time.LocalDate;
+
 import com.alibaba.excel.converters.Converter;
 import com.alibaba.excel.enums.CellDataTypeEnum;
 import com.alibaba.excel.metadata.GlobalConfiguration;
@@ -25,7 +28,15 @@ public class ExcelDate2TimestampConverter implements Converter<Integer> {
 
     @Override
     public Integer convertToJavaData(ReadCellData<?> cellData, ExcelContentProperty contentProperty, GlobalConfiguration globalConfiguration) throws Exception {
-        String stringValue = cellData.getStringValue();
-        return Long.valueOf(DateUtils.dateToUnixTimestamp(stringValue, DateUtils.DATE_FULL_STR) / 1000).intValue();
+        if(cellData.getType().equals(CellDataTypeEnum.NUMBER)){
+            LocalDate localDate = LocalDate.of(1900, 1, 1).plusDays(cellData.getNumberValue().longValue() - 2);
+            Long time = Date.valueOf(localDate).getTime() / 1000;
+            return time.intValue();
+        }
+        if(cellData.getType().equals(CellDataTypeEnum.STRING)){
+            String stringValue = cellData.getStringValue();
+            return Long.valueOf(DateUtils.dateToUnixTimestamp(stringValue, DateUtils.DATE_FULL_STR) / 1000).intValue();
+        }
+        return null;
     }
 }
